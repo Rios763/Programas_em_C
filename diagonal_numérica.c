5343de9c15e57b7a56d8d1e60b33c3ccdc3a89f9
Gabriel Rios <127359852+Rios763@users.noreply.github.com>
2023-05-08 18:12:57 -0300

    Update diagonal_numérica.c

--- diagonal_numérica.c
+++ diagonal_numérica.c
@@ -1,39 +1,43 @@
 /*
 ENUNCIADO
 
-    faca um programa que leia um numero n e
-    imprima n linhas na tela com seguinte formato (exemplo para n=6)
-    
+    Faça um programa que leia um inteiron(no máximo 50) e imprima uma saída da forma:
+
     1
-    1 2
-    1 2 3
-    1 2 3 4
-    1 2 3 4 5
-    1 2 3 4 5 6
+     2
+      3
+       4
+        5
+
 */
 
-
-#include stdio.h
-#include stdlib.h
+#include <stdio.h>
+#include <stdlib.h>
 
 
 int main()
 {
 
     int valor_n;
-    printf(Insira um valor inteiro nnt);
-    scanf(%d,&valor_n);
-    printf(nn);
-    for (int i=1; i=valor_n;i++){
-            for (int j=1; j=i; j++){
-                printf(%2d ,j);
-            }
-        printf(n);
+    printf("Insira um valor inteiro 'n' (menor do que 50): \n\t");
+    scanf("%d",&valor_n);
+
+    if(valor_n > 50){
+        printf("Valor invalido!");
+        return 0;
+    }
+
+
+    for(int i=1; i<=valor_n;i++){
+        for(int c=1;c<=valor_n; c++){
+            if(c == i)
+            printf("%d ", c);
+            else printf(" ");
+
+        }
+        printf("\n");
     }
 
 
     return 0;
 }
-
-
-
